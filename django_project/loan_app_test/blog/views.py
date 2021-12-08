6e537730aec41d597ddc28255bd4c547e329b680
NeyoCreator <neos25722@gmail.com>
2021-12-08 03:47:35 +0200

    Added a Post model, where users posts can be updated to the database

--- django_project/loan_app_test/blog/views.py
+++ django_project/loan_app_test/blog/views.py
@@ -1,28 +1,11 @@
 from django.shortcuts import render
-
-
-#Dummy data 
-posts = [
-    {'author': 'Neo Sebanze',
-    'title': 'Loan Application',
-    'content': 'Need 700',
-    'date_posted': 'August 27,2018'
-
-    },
-
-     {'author': 'Karabo Masemula',
-    'title': 'Loan Application',
-    'content': 'Need 200',
-    'date_posted': 'August 30,2018'
-
-    }
-
-]
+from .models import Post  
+ 
 
 
 def home(request):
     context = {
-        "posts":posts
+        "posts":Post.objects.all()
         }
     return render(request, 'blog/home.html',context)
 
